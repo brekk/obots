0762aa39827e73f60ccb11f96bd015c2a7a250c2
brekk <brekk@brekkbockrath.com>
2017-12-27 09:14:35 -0800

    es6ify

--- src/rgb.js
+++ src/rgb.js
@@ -1,23 +1,29 @@
-// import and name a constant called five
-const {map, freeze, curry} = require('f-utility')
-const five = require(`johnny-five`)
-const color = curry(require('colortransition'))
-const _debounce = require('lodash.debounce')
-const bug = require('debug')
-const {debug} = require('xtrace')
+import {
+  map,
+  freeze,
+  curry
+} from 'f-utility'
+import five from 'johnny-five'
+import _color from 'colortransition'
+import _debounce from 'lodash.debounce'
+import bug from 'debug'
 
-const [photoCellLog, lightLog, buttonLog] = map(bug, [
+const color = curry(_color)
+const debounce = curry((amount, fn) => _debounce(fn, amount))
+
+const [
+  photoCellLog,
+  lightLog
+  // buttonLog
+] = map(bug, [
   `obots:photocell`,
   `obots:led`,
   `obots:button`
 ])
 
-// _debounce(fn, amount)
-const debounce = curry((amount, fn) => _debounce(fn, amount))
-
 const board = five.Board()
 const SENSOR_DEBOUNCE_INTERVAL = 50
-const BLINK_SPEED = 100
+// const BLINK_SPEED = 100
 const COLOR_SCALE = 4
 const LOW_INTENSITY = 30
 const COLORS = freeze({
@@ -25,27 +31,30 @@ const COLORS = freeze({
   TURQUOISE: `#00ccff`,
   MAGENTA: `#ff0088`
 })
-const runFromColorToColor = curry((start, end, intervalInMs, cb) => {
-  const changeColorByPercentage = color(start, end)
-  let count = 0
-  let counter = COLOR_SCALE
-  const THE_LIMIT = 100
-  return setInterval(() => {
-    lightLog(`counting! ${count} + ${counter}`, count + counter)
-    count += counter
-    if (count > THE_LIMIT || count <= 0) {
-      counter *= -1
-    }
-    if (count <= 0) {
-      count = 0
-    }
-    if (count >= THE_LIMIT) {
-      count = THE_LIMIT
-    }
-    // count %= THE_LIMIT
-    cb(changeColorByPercentage(count))
-  }, intervalInMs)
-})
+// express this with sine!
+const runFromColorToColor = curry(
+  (start, end, intervalInMs, cb) => {
+    const changeColorByPercentage = color(start, end)
+    let count = 0
+    let counter = COLOR_SCALE
+    const THE_LIMIT = 100
+    return setInterval(() => {
+      lightLog(`counting! ${count} + ${counter}`, count + counter)
+      count += counter
+      if (count > THE_LIMIT || count <= 0) {
+        counter *= -1
+      }
+      if (count <= 0) {
+        count = 0
+      }
+      if (count >= THE_LIMIT) {
+        count = THE_LIMIT
+      }
+      // count %= THE_LIMIT
+      cb(changeColorByPercentage(count))
+    }, intervalInMs)
+  }
+)
 const runFromOneToTwo = runFromColorToColor(COLORS.MAGENTA, COLORS.TURQUOISE)
 const runFromOneToTwoFast = runFromOneToTwo(300)
 
@@ -65,7 +74,7 @@ const callbackWhenBoardReady = () => {
 
   const startLightLoop = () => {
     if (!isOn) {
-      led.color(`#ff0088`)
+      led.color(COLORS.MAGENTA)
       led.intensity(LOW_INTENSITY)
       isOn = true
       killId = runFromOneToTwoFast((newColor) => {
@@ -90,11 +99,8 @@ const callbackWhenBoardReady = () => {
   //   // console.log(`specifically, 0 - 0.3`)
   // })
   function photoCellChange() {
-    // const level = 0.5
-    // const this = {level}
-    // const level = this.level
     const {level} = this
-    if (level > 0.75) {
+    if (level > 0.85) {
       photoCellLog(`NO LIGHT!`, Date.now())
       if (!isOn) {
         startLightLoop()
@@ -129,14 +135,6 @@ const callbackWhenBoardReady = () => {
   // #RRGGBB
   // #F0F
   // #FF00FF
-
-  const log = console.log.bind(console)
-  const returnRandomColor = () => {
-    const MAX_COLOR_CONSTANT = 16777215
-    return '#'+Math.floor(
-      Math.random() * MAX_COLOR_CONSTANT
-    ).toString(16)
-  }
 
   button.on(`down`, startLightLoop)
 
---
+++ wallaby.js
@@ -0,0 +1,64 @@
+const pkg = require(`./package.json`)
+
+module.exports = function configureWallaby() {
+  return {
+    name: pkg.name,
+    debug: true,
+    files: [
+      `src/*.js`,
+      `!src/*.spec.js`
+    ],
+
+    tests: [
+      `src/*.spec.js`,
+      // wallaby doesn't know about this yet
+      `!src/bundle.spec.js`
+    ],
+
+    env: {
+      type: `node`,
+      runner: `node`
+    },
+
+    // compilers: {
+    //   '**/*.js': wallaby.compilers.babel()
+    // },
+    preprocessors: {
+      '**/*.js': (file) => require(`babel-core`).transform(
+        file.content, {
+          sourceMap: true,
+          presets: [`es2015`],
+          plugins: [`transform-object-rest-spread`]
+        }
+      )
+    },
+
+    testFramework: `jest`,
+
+    setup: function setupWallaby(w) {
+      require(`babel-polyfill`) // eslint-disable-line fp/no-unused-expression
+      w.testFramework.configure({
+        "modulePaths": [
+          `src`
+        ],
+        "moduleDirectories": [
+          `node_modules`,
+          `src`
+        ],
+        "mapCoverage": true,
+        "moduleFileExtensions": [
+          `js`,
+          `json`
+        ],
+        "testMatch": [
+          `**/*.spec.(jsx|js)`
+        ]
+      })
+    },
+
+    debug: true,
+    filesWithNoCoverageCalculated: [
+      // `src/core/fs.js`
+    ]
+  }
+}
