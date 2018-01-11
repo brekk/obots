490416d561e115a4fec3a80263444c396db007d1
brekk <brekk@brekkbockrath.com>
2018-01-10 22:30:36 -0800

    shroomy updates for mega

--- src/circuits/shroomy.js
+++ src/circuits/shroomy.js
@@ -2,27 +2,31 @@ import five from 'johnny-five'
 import {
   e0
 } from 'entrust'
-import {
-  isNumber,
-  map,
-  ternary
-} from 'f-utility'
+import {COLORS} from '../light'
+const K = (x) => () => x
+// import {
+//   isNumber,
+//   map,
+//   ternary
+// } from 'f-utility'
 const LIGHTS = {
   rgb1: {
-    red: 11,
-    green: 10,
-    blue: 9
+    red: 3,
+    green: 5,
+    blue: 6
   },
   rgb2: {
-    red: 6,
-    green: 5,
-    blue: 3
+    red: 9,
+    green: 10,
+    blue: 11
   },
-  littleGreen1: 4,
-  littleGreen2: 2,
-  green: 12,
-  yellow: 8,
-  red: 7
+  littleGreen1: 25,
+  littleGreen2: 22,
+  green: 23,
+  yellow: 27,
+  red: 24,
+  white: 28,
+  blue: 26
 }
 /*
 rgb1 => magentas & pinks & pale pink
@@ -39,6 +43,8 @@ board.on(`ready`, function boardReady() {
   const littleGreen1 = new five.Led(LIGHTS.littleGreen1)
   const littleGreen2 = new five.Led(LIGHTS.littleGreen2)
   const green = new five.Led(LIGHTS.green)
+  const blue = new five.Led(LIGHTS.blue)
+  const white = new five.Led(LIGHTS.white)
   const red = new five.Led(LIGHTS.red)
   const yellow = new five.Led(LIGHTS.yellow)
   const rgb1 = new five.Led.RGB({pins: LIGHTS.rgb1})
@@ -76,38 +82,60 @@ board.on(`ready`, function boardReady() {
     red,
     yellow,
     rgb1,
-    rgb2
+    rgb2,
+    white,
+    blue
   })
 
-  // Turn it on and set the initial color
-  // on(littleGreen1)
-  // littleGreen1.on()
-  // green.on()
-  // on(littleGreen1)
-  // green => littleGreen2 => yellow => littleGreen1 => red
-  on(green)
+  // lg1 => rgb2(purple) => red => green =>
+  // white => rgb1(magenta) => blue => yellow => lg2
+  const S = [
+    littleGreen1,
+    rgb2,
+    red,
+    green,
+    white,
+    rgb1,
+    littleGreen2,
+    yellow,
+    blue
+  ]
+  on(S[0])
   setTimeout(() => {
-    off(green)
-    on(littleGreen2)
+    off(S[0])
+    // on(S[1])
+    rgb2.color(COLORS.PURPLE)
+    rgb2.intensity(8)
   }, 1000)
   setTimeout(() => {
-    off(littleGreen2)
-    on(yellow)
+    off(S[1])
+    // rgb2.off()
+    on(S[2])
   }, 2000)
   setTimeout(() => {
-    off(yellow)
-    on(littleGreen1)
+    off(S[2])
+    on(S[3])
   }, 3000)
   setTimeout(() => {
-    off(littleGreen1)
-    on(red)
+    off(S[3])
+    on(S[4])
   }, 4000)
   setTimeout(() => {
-    off(red)
-    rgb1.color(`#ff00ff`)
+    off(S[4])
+    rgb1.color(COLORS.MAGENTA)
+    rgb1.intensity(12)
+    // on(S[5])
   }, 5000)
   setTimeout(() => {
-    off(rgb1)
-    rgb2.color(`#ffff00`)
+    off(S[5])
+    on(S[6])
   }, 6000)
+  setTimeout(() => {
+    off(S[6])
+    on(S[7])
+  }, 7000)
+  setTimeout(() => {
+    off(S[7])
+    on(S[8])
+  }, 8000)
 })
