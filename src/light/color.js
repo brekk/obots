211167712bdd99cb7e35ecb1bd4f31bdd52992d8
brekk <brekk@brekkbockrath.com>
2018-01-20 08:39:30 -0800

    controller!

--- src/light/color.js
+++ src/light/color.js
@@ -1,5 +1,8 @@
-export const random = () => (
-  `#` + Math.floor(
-    Math.random() * 16777215
-  ).toString(16)
-)
+export const random = () => {
+  const core = Math.random() * (Math.pow(2, 24) - 1)
+  let root = Math.floor(core).toString(16)
+  while (root.length < 6) {
+    root += `0` // eslint-disable-line
+  }
+  return `#` + root
+}
